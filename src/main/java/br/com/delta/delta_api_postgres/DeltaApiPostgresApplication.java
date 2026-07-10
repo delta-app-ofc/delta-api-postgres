36ae7a7e4e17c7d78265e7ff99c9b8886ebe8427
JoaoSouza <jparaujo0409@gmail.com>
2026-07-10 20:49:38 -0300

    chore: inicializa projeto Spring Boot

---
+++ src/main/java/br/com/delta/delta_api_postgres/DeltaApiPostgresApplication.java
@@ -0,0 +1,13 @@
+package br.com.delta.delta_api_postgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeltaApiPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeltaApiPostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/delta/delta_api_postgres/DeltaApiPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.delta.delta_api_postgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeltaApiPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
