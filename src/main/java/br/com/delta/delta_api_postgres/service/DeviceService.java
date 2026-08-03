5fc1f3fe47902198edb732740528254793089029
JoaoSouza <jparaujo0409@gmail.com>
2026-08-03 12:04:03 -0300

    feat: estrutura inicial service

--- src/main/java/br/com/delta/delta_api_postgres/service/DeviceService.java
+++ src/main/java/br/com/delta/delta_api_postgres/service/DeviceService.java
@@ -1,4 +1,10 @@
 package br.com.delta.delta_api_postgres.service;
 
+import br.com.delta.delta_api_postgres.repository.DeviceRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DeviceService {
+    private final DeviceRepository deviceRepository;
+
 }
