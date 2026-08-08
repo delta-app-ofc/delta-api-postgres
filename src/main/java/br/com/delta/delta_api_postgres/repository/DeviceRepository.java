a08de67efbefba85e953e53bd92eb977f150d885
JoaoSouza <jparaujo0409@gmail.com>
2026-08-08 17:44:50 -0300

    fix: ajusta coluna string nos repository methods

--- src/main/java/br/com/delta/delta_api_postgres/repository/DeviceRepository.java
+++ src/main/java/br/com/delta/delta_api_postgres/repository/DeviceRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface DeviceRepository extends JpaRepository<Device, Integer> {
-    Optional<Device> findByDeviceId(Integer deviceId);
+    Optional<Device> findByDeviceId(String deviceId);
 
-    boolean existsByDeviceId(Integer deviceId);
+    boolean existsByDeviceId(String deviceId);
 }
