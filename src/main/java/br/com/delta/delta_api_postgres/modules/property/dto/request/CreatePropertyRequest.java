692a259dcc29d1417dddf0a4a044ee33b2d4cf35
JobasStuff <jparaujo0409@gmail.com>
2026-08-28 20:36:41 -0300

    feat: adiciona validações no createDTO

--- src/main/java/br/com/delta/delta_api_postgres/modules/property/dto/request/CreatePropertyRequest.java
+++ src/main/java/br/com/delta/delta_api_postgres/modules/property/dto/request/CreatePropertyRequest.java
@@ -2,11 +2,19 @@ package br.com.delta.delta_api_postgres.modules.property.dto.request;
 
 import br.com.delta.delta_api_postgres.modules.property.enums.PropertyClassification;
 import br.com.delta.delta_api_postgres.modules.property.enums.PropertyType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public record CreatePropertyRequest(
-    String name,
-    PropertyType type,
-    PropertyClassification classification,
-    Integer addressId
+        @NotBlank @Size(max = 100)
+        String name,
+        @NotNull
+        PropertyType type,
+        @NotNull
+        PropertyClassification classification,
+        @NotNull @Positive
+        Integer addressId
 ) {
 }
--- src/main/java/br/com/delta/delta_api_postgres/modules/property/service/PropertyService.java
+++ src/main/java/br/com/delta/delta_api_postgres/modules/property/service/PropertyService.java
@@ -1,5 +1,9 @@
 package br.com.delta.delta_api_postgres.modules.property.service;
 
+import br.com.delta.delta_api_postgres.modules.property.dto.io.PropertyIO;
+import br.com.delta.delta_api_postgres.modules.property.dto.request.CreatePropertyRequest;
+import br.com.delta.delta_api_postgres.modules.property.entity.Property;
+import br.com.delta.delta_api_postgres.modules.property.mapper.PropertyMapper;
 import br.com.delta.delta_api_postgres.modules.property.repository.PropertyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,6 +12,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PropertyService {
     private final PropertyRepository propertyRepository;
+    private final PropertyMapper propertyMapper;
 
+    public PropertyIO create(CreatePropertyRequest request) {
+        PropertyIO io = propertyMapper.fromCreateToIO(request);
 
+        Property property = propertyRepository.save(propertyMapper.toEntity(io));
+
+        return propertyMapper.toIO(property);
+    }
 }
