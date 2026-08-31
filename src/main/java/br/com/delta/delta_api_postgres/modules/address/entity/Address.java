8d321cec1e26777da8922ab247330e5cf6f31ab2
JobasStuff <jparaujo0409@gmail.com>
2026-08-31 10:35:19 -0300

    feat: adiciona entity e modulos complementares

---
+++ src/main/java/br/com/delta/delta_api_postgres/modules/address/entity/Address.java
@@ -0,0 +1,29 @@
+package br.com.delta.delta_api_postgres.modules.address.entity;
+
+import br.com.delta.delta_api_postgres.modules.region.entity.Region;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_address")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id", nullable = false)
+    private Region region;
+    @Column(name = "cep", nullable = false, length = 8)
+    private String cep;
+    @Column(name = "city", nullable = false, length = 60)
+    private String city;
+    @Column(name = "state", nullable = false, length = 30)
+    private String state;
+}
---
+++ src/main/java/br/com/delta/delta_api_postgres/modules/region/entity/Region.java
@@ -0,0 +1,15 @@
+package br.com.delta.delta_api_postgres.modules.region.entity;
+
+import br.com.delta.delta_api_postgres.modules.region.enums.RegionName;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_region")
+public class Region {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "name", nullable = false, unique = true, length = 20)
+    private RegionName name;
+}
