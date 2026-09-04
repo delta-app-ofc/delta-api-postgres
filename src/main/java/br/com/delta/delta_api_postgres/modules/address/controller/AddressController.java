7d234a749b09e5a2bace058b804c86da6d1d0d80
JobasStuff <jparaujo0409@gmail.com>
2026-09-04 10:54:10 -0300

    feat: adiciona implements do swagger no controller

--- src/main/java/br/com/delta/delta_api_postgres/modules/address/controller/AddressController.java
+++ src/main/java/br/com/delta/delta_api_postgres/modules/address/controller/AddressController.java
@@ -5,6 +5,7 @@ import br.com.delta.delta_api_postgres.modules.address.dto.request.CreateAddress
 import br.com.delta.delta_api_postgres.modules.address.dto.request.UpdateAddressRequest;
 import br.com.delta.delta_api_postgres.modules.address.mapper.AddressMapper;
 import br.com.delta.delta_api_postgres.modules.address.service.AddressService;
+import br.com.delta.delta_api_postgres.modules.address.swagger.AddressSwagger;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,10 +17,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/delta/address")
 @RequiredArgsConstructor
-public class AddressController {
+public class AddressController implements AddressSwagger {
     private final AddressService addressService;
     private final AddressMapper addressMapper;
 
+    @Override
     @PostMapping
     public ResponseEntity<AddressIO> create(
             @Valid @RequestBody CreateAddressRequest request){
@@ -30,25 +32,26 @@ public class AddressController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
-
+    @Override
     @GetMapping
     public ResponseEntity<List<AddressIO>> findAll(){
         List<AddressIO> response = addressService.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
-
+    @Override
     @GetMapping("/{id}")
     public ResponseEntity<AddressIO> findById(@PathVariable Integer id){
         AddressIO response = addressService.findById(id);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
-
+    @Override
     @PutMapping("/{id}")
     public ResponseEntity<AddressIO> update(@PathVariable Integer id, @RequestBody @Valid UpdateAddressRequest updateAddressRequest){
         AddressIO response = addressService.update(id, addressMapper.fromUpdateRequest(id, updateAddressRequest));
 
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
+    @Override
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Integer id){
         addressService.delete(id);
