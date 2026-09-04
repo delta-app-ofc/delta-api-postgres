e69c959b68decf777e69afe715567178abbbecdb
JobasStuff <jparaujo0409@gmail.com>
2026-09-04 07:24:41 -0300

    feat: adiciona regra de negócio no service

--- src/main/java/br/com/delta/delta_api_postgres/modules/address/service/AddressService.java
+++ src/main/java/br/com/delta/delta_api_postgres/modules/address/service/AddressService.java
@@ -8,8 +8,11 @@ import br.com.delta.delta_api_postgres.modules.address.mapper.AddressMapper;
 import br.com.delta.delta_api_postgres.modules.address.repository.AddressRepository;
 import br.com.delta.delta_api_postgres.modules.region.entity.Region;
 import br.com.delta.delta_api_postgres.modules.region.repository.RegionRepository;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,14 +22,45 @@ public class AddressService {
     private final AddressMapper addressMapper;
 
     public AddressIO create(AddressIO addressIO) {
-            Region region = regionRepository.findById(addressIO.regionId()).orElseThrow(
-                    () -> new ResourceNotFoundException("Região nao encontrada")
-            );
+        Region region = regionRepository.findById(addressIO.regionId()).orElseThrow(
+                () -> new ResourceNotFoundException("Região nao encontrada")
+        );
+        Address address = addressMapper.toEntity(addressIO, region);
 
-            Address address = addressMapper.toEntity(addressIO, region);
+        Address savedAddress = addressRepository.save(address);
 
-            Address savedAddress = addressRepository.save(address);
+        return addressMapper.toIo(savedAddress);
+    }
+    @Transactional(readOnly = true)
+    public List<AddressIO> findAll(){
+        return addressRepository.findAll().stream().map(addressMapper::toIo).toList();
+    }
 
-            return addressMapper.toIo(savedAddress);
-        }
+    public AddressIO findById(Integer id) {
+        Address address = addressRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Endereço nao encontrado")
+        );
+
+        return addressMapper.toIo(address);
+    }
+    public AddressIO update(Integer id, AddressIO addressIO) {
+        Address address = addressRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Endereço nao encontrado")
+        );
+
+        Region region = regionRepository.findById(addressIO.regionId()).orElseThrow(
+                () -> new ResourceNotFoundException("Região nao encontrada")
+        );
+
+        addressMapper.updateEntity(address, addressIO, region);
+
+        return addressMapper.toIo(addressRepository.save(address));
+    }
+    public void delete(Integer id) {
+        Address address = addressRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Endereço nao encontrado")
+        );
+
+        addressRepository.delete(address);
+    }
 }
